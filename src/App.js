14a43de4db97a0bdd4e770d44d265b6667ac67b6
unknown <crsiufung@hotmail.com>
2022-03-28 19:32:49 +0800

    routing with React-Router

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
-import CategoriesContainer from "./components/categories-container/categories-container.component";
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./components/routes/home/home.component";
+import Navigation from "./components/routes/navigation/navigation.component";
+import SignIn from "./components/routes/sign-in/sign-in.component";
+
+const Shop = () => {
+  return (
+    <div>I am the Shop</div>
+  )
+}
 
 const App = () => {
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-
   return (
-      <CategoriesContainer categories={categories}/>
-  );
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
